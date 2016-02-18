a0feeb371d7f98db9b6304467098f54c7f0e15ec
carbon <standardprogram@foxmail.com>
2016-02-18 12:47:49 +0800

    fix db access problem, add db, mem key in server configuration

--- app/playermanager.py
+++ app/playermanager.py
@@ -5,15 +5,19 @@ from gfirefly.dbentrust.madminanager import MAdminManager
 from gfirefly.dbentrust.mmode import MAdmin
 
 
-def __init__(self):
-    MAdminManager().registe("GameData")
-    self.tbplayer = MAdmin("user", "uid")
 
 @remoteserviceHandle("gate")
 def GetPlayerData(uid):
     print(uid)
+    MAdminManager().registe("GameData")
 
-    playerData = self.tbplayer.getObjData(uid)   
+    tbplayer = MAdmin("user", "uid")
+    print(tbplayer)
+    playerData = tbplayer.getObjData(uid)
+    print(playerData)
+    
+    
+    
     return json.dumps(playerData)
 
     
